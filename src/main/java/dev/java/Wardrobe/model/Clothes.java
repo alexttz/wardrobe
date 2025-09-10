6f3ecd2cda9d81165f789972ca50f6b52855e8e7
alexttz <lima.alexander.las@gmail.com>
2025-09-10 17:33:08 -0300

    Finalizada a criação do modelo e gerado os Gets e Sets

--- src/main/java/dev/java/Wardrobe/model/Clothes.java
+++ src/main/java/dev/java/Wardrobe/model/Clothes.java
@@ -8,11 +8,44 @@ import jakarta.persistence.*;
 public class Clothes {
 
     @Id // Definimos que o primeiro item será o ID da roupa
-    @GeneratedValue (strategy = GenerationType.IDENTITY) //PAREI AQUI
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // E aqui definimos que o ID será gerado automaticamente quando for criado
     private long id; // Id único da roupa
     private String brand; // Marca da roupa
     private String color; // Cor da roupa
     private String model; // Modelo (ex: Regata)
     private String size; // Tamanho (ex: Large, Medium, Small, Extra Large)
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    
 }
 
